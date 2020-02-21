8b8775c357682f1fefc17c0d0e83d2ecab08659a
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-02-21 09:34:38 -0300

    Create Owner models and dependencies

---
+++ core/enum.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+from enum import Enum
+
+choice = namedtuple('Choice', 'name value')
+
+
+class ChoiceEnum(Enum):
+    @classmethod
+    def choices(cls):
+        return [choice(e.name, e.value) for e in cls]
+
+
+class Gender(ChoiceEnum):
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
+    NA = 'NA'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from core.enum import Gender
+
+
+class ReligiousOrder(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=128)
+    original_name = models.CharField(max_length=128)
+    spouse_name = models.CharField(max_length=128)
+    gender = models.CharField(max_length=128, choices=Gender.choices())
+    indian = models.BooleanField(default=False)
+    captaincy_resident = models.BooleanField(default=True)
+    secular_clergy = models.BooleanField(default=False)
+    regular_clergy = models.BooleanField(default=False)
+    comments = models.TextField()
+    privileged_observations = models.TextField()
+    religious_order = models.ForeignKey(
+        ReligiousOrder, on_delete=models.SET_NULL, null=True
+    )
