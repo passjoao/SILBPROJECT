4efdd39da72e1d4f690e42dc17f52c2159b2a095
amaro <j.amarorn@gmail.com>
2020-06-02 18:26:09 -0300

    metodo de pesquisa

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Request, Owner
+from django.http import HttpResponse
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Request, Owner, LandRecord
 
 def index(request):
     """View function for home page of site."""
@@ -37,3 +42,27 @@ class RequestListViews(SearchListView):
 
 class RequestDetailView(generic.DetailView):
     model = Request
+
+@csrf_exempt
+def loaddados(request):
+    a = []
+    if (request.method == "POST"):
+        body_unicode = eval(request.body.decode('utf-8'))
+        pesquisa = body_unicode['pesquisa']
+        landrecord = LandRecord.objects.filter(reference__contains=pesquisa,location__contains=pesquisa)
+        owner = Owner.objects.filter(name__contains=pesquisa,original_name__contains=pesquisa)
+        request = Request.objects.filter(requestType__contains=pesquisa)
+        f = max([landrecord.count(),owner.count(),request.count()],key=int)
+    return HttpResponse(json.dumps(a),content_type='application/json',status=201)
+
+#     dados = []
+#     if (request.method == "POST"):
+#         body_unicode = eval(request.body.decode('utf-8'))
+#         #referencia landrecord - reference
+#         re = LandRecord.objects.filter(reference = )
+#     #referencia landrecord - reference
+#     #localização landrecord - location
+#     #Data da Requisição request - dateRequest
+#     #Data da concessão confirmation - dateConfirmation
+#     #Sesmeiros owner - name, original_name
+    #     #Tipo de carta request - requestType
