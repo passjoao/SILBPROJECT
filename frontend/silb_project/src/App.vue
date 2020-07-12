2e4128f1131312493d2cb9baa976963280db7567
amaro <j.amarorn@gmail.com>
2020-07-12 13:29:12 -0300

    add iginore

--- frontend/silb_project/src/App.vue
+++ frontend/silb_project/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/cadastro">Cadadstro de Usuário</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="flex items-center bg-gray-600 p-3 flex-wrap">
+        <a href="" class="p-2 mr-4 inline-flex items-center">
+          SILB
+        </a>
+        <button class="text-white flex-initial p-3 hover:bg-gray-900 rounded lg:hidden">
+          <b-icon icon="list"  variant="secondary" >menu</b-icon>
+        </button>
+        <div class="top-nav w-full lg:inline-flex lg:flex-grow lg:w-auto" id="navigation">
+          <div class="lg:inline-flex lg:flex-row lg:ml-auto">
+            <a href="" class="lg:inline-flex lg:w-auto px-3 py-2 rounded text-gray-400">
+              <span>
+                Banco
+              </span>
+            </a>
+            <a href="" class="lg:inline-flex lg:w-auto px-3 py-2 rounded text-gray-400">
+              <span>
+                Sobre
+              </span>
+            </a>
+            <a href="" class="lg:inline-flex lg:w-auto px-3 py-2 rounded text-gray-400">
+              <span>
+                Contato
+              </span>
+            </a>
+          </div>
+        </div>
+      </nav>
     </div>
     <router-view/>
   </div>
@@ -14,13 +38,11 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  //text-align: center;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
