c0a12f392c1a283a8887f6915bbdc2eba643d67f
amaro <j.amarorn@gmail.com>
2020-07-17 19:15:40 -0300

    criação de pagina de Banco #25

--- frontend/silb_project/src/views/Banco.vue
+++ frontend/silb_project/src/views/Banco.vue
@@ -1,12 +1,75 @@
 <template>
 <div>
-  <h1>Banco</h1>
+  <table class="table-auto">
+    <thead>
+    <tr>
+      <th class="px-4 py-2">Referência</th>
+      <th class="px-4 py-2">Sesmeiro</th>
+      <th class="px-4 py-2">Data Requerimento</th>
+      <th class="px-4 py-2">Data de Concessão</th>
+      <th class="px-4 py-2">Localidade</th>
+      <th class="px-4 py-2">Área(léguas)</th>
+      <th class="px-4 py-2">Data de Confirmação</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td class="border px-4 py-2">Intro to CSS</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">858</td>
+      <td class="border px-4 py-2">858</td>
+      <td class="border px-4 py-2">858</td>
+      <td class="border px-4 py-2">858</td>
+      <td class="border px-4 py-2">858</td>
+    </tr>
+    <tr class="bg-gray-100">
+      <td class="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">112</td>
+      <td class="border px-4 py-2">112</td>
+      <td class="border px-4 py-2">112</td>
+      <td class="border px-4 py-2">112</td>
+      <td class="border px-4 py-2">112</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Intro to JavaScript</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+      <td class="border px-4 py-2">1,280</td>
+      <td class="border px-4 py-2">1,280</td>
+      <td class="border px-4 py-2">1,280</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    </tbody>
+  </table>
+
+  <div class="bg-purple-300">
+    <h1>teste
+    <span>
+      <a href="">
+        +
+      </a>
+    </span>
+    </h1>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-name: "Banco"
+name: "Banco",
+  data() {
+    return{
+    itens:[{}],
+  }
+
+
+  },
+  methods:{
+
+
+  }
+
 }
 </script>
 
