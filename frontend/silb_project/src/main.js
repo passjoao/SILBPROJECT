ae82f9624d266b819e5dee924e15eaf876fabb14
Jose Amaro <neto@MacBook-Pro-de-Jose.local>
2020-08-31 21:24:11 -0300

    Ath login firebase

--- frontend/silb_project/src/main.js
+++ frontend/silb_project/src/main.js
@@ -5,8 +5,22 @@ import './assets/css/tailwind.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import axios from 'axios'
 import moment from 'moment'
-  
+import firebase from "firebase";
+
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8d7qyPFi_2otiTaZTEAyjv08qJpUJDxw",
+  authDomain: "silbproject.firebaseapp.com",
+  databaseURL: "https://silbproject.firebaseio.com",
+  projectId: "silbproject",
+  storageBucket: "silbproject.appspot.com",
+  messagingSenderId: "557455423181",
+  appId: "1:557455423181:web:6616f5a47e1c3d2c3c5b84",
+  measurementId: "G-TNTPH8ZS26"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
--- frontend/silb_project/src/views/Login.vue
+++ frontend/silb_project/src/views/Login.vue
@@ -1,22 +1,22 @@
 <template>
 	<div class="login">
-	<form>
+	<form action="post">
     <p class="h4 text-center mb-4">Entrar na SILB</p>
     <label for="defaultFormLoginEmailEx" class="grey-text">E-mail:</label>
-    <input type="email" id="defaultFormLoginEmailEx" class="form-control" placeholder="E-mail" v-model="email" />
+    <input type="text" id="defaultFormLoginEmailEx" class="form-control" placeholder="E-mail" v-model="email" />
     <br/>
     <label for="defaultFormLoginPasswordEx" class="grey-text">Senha:</label>
     <input type="password" id="defaultFormLoginPasswordEx" class="form-control" placeholder="Senha" v-model="senha" />
     <router-link to='/'>esqueceu a senha </router-link>
     <div class="text-center mt-4 blue">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click="login">Entrar</button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click.prevent="login">Entrar</button>
     </div>
   </form>
   </div>
 </template>
 
 <script>
-	
+import firebase from "firebase";
 	export default{
 		name: "login",
 		data() {
@@ -26,6 +26,18 @@
 			};
 		},
 		methods: {
+      login () {
+        return firebase.auth().signInWithEmailAndPassword(this.email,this.senha).then(
+            user =>{
+              console.log(user)
+              alert(user)
+            },
+            err => {
+              // alert(this.erro[err.code])
+              console.log(err)
+            }
+        )
+      }
 		}
 			
 		
